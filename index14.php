1a8f0d23cfcac6e859ffa15a24c7fc1124dcbb9a
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-03-20 00:31:28 +0530

    functions

---
+++ index14.php
@@ -0,0 +1,9 @@
+<?php
+    function happy_birthday($first_name){
+        echo "Happy birthday to {$first_name}<br>";
+        echo "May god bless you<br>";
+        echo "Happy birthday to {$first_name}<br>";
+    }
+    happy_birthday("Aarush");
+
+?>
