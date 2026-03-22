9a066c5cd49231623c77e0abe0924a58bddd1c76
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-03-22 15:35:39 +0530

    string functions

---
+++ index15.php
@@ -0,0 +1,16 @@
+<?php
+    $username = "Bro Code";
+    //$username = strtolower()
+    // strtoupper
+    //trim => removes whitespaces
+    //str_replace("-","",$phone)
+    //strrev => reverse
+    //str_shuffle = 
+    //strcmp ($var, "string") if both are same this returns 0
+    //strlen -> returns the length of the string
+    //strpos($number, "1")
+    // $firstname = substr($username, 0, 3);
+    //explode turns string into an array
+    // implode turns an array into a string 
+    
+?>
